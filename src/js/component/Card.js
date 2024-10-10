faa2850b0eaa46d4a14f0fae683282a0f99661ac
WistonTravieso <wistontravieso@gmail.com>
2024-10-10 03:28:35 +0000

    [Agregar Props en el componente Card]

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Cards from './Cards'
 
-const Card = () => {
+const Card = ({image, title, description}) => {
     return (
         <div className="card text-center" style={{width: "18rem"}}>
-            <img src="https://images.unsplash.com/photo-1722903029367-5ed163ae5fef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
+            <img src={image} alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
                     <a href="#" className="btn btn-primary">Find out More!</a>
                 </div>
         </div>
--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -5,16 +5,28 @@ const Cards = () => {
     return (
         <div className="row m-2">
             <div className="col">
-                <Card />
+                <Card image="https://images.unsplash.com/photo-1722903029367-5ed163ae5fef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8NTAwJTIweCUyMDMyNXxlbnwwfHwwfHx8MA%3D%3D"
+                    title="Card 1"
+                    description="Description Card 1" />
             </div>
             <div className="col">
-                <Card />
+                <Card image="https://images.unsplash.com/photo-1727378743499-74a47a1feec4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8NTAwJTIweCUyMDMyNXxlbnwwfHwwfHx8MA%3D%3D"
+                title="Card 2"
+                description="Description Card 2"
+                />
             </div>
             <div className="col">
-                <Card />
+                <Card image="https://images.unsplash.com/photo-1722096703718-a04f14ecde52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8NTAwJTIweCUyMDMyNXxlbnwwfHwwfHx8MA%3D%3D"
+                title="Card 3"
+                description="Description Card 3"
+                />
             </div>
             <div className="col">
-                <Card />
+                <Card 
+                image="https://images.unsplash.com/photo-1727461442528-5b0b98644c66?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8NTAwJTIweCUyMDMyNXxlbnwwfHwwfHx8MA%3D%3D"
+                title="Card 4"
+                description="Description Card 4"
+                />
             </div>
         </div>
     )
